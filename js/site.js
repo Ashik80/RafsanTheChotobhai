55fc37bc34846667cf72181ec187815199edea28
Ashik80 <ashikurrahman020995@gmail.com>
2020-03-19 01:12:23 +0600

    resp

--- js/site.js
+++ js/site.js
@@ -1,23 +1,53 @@
-$(document).ready(function(){
-    setInterval(function(){
-        $('.words-wrapper').css({"width":"113px"});
-    },2000);
-    setInterval(function(){
-        $('.words-wrapper').css({"width":"0px"});
-    },4000);
+$(document).ready(function () {
+    setInterval(function () {
+        $('.words-wrapper').css({
+            "width": "113px"
+        });
+    }, 2000);
+    setInterval(function () {
+        $('.words-wrapper').css({
+            "width": "0px"
+        });
+    }, 4000);
 
-    var text = ['Love','Lead'];
+    var text = ['Love', 'Lead'];
     var counter = 0;
-    var int = setInterval(change,4500);
+    var int = setInterval(change, 4500);
+
     function change() {
         $('.words-wrapper').text(text[counter]);
         counter++;
-        if(counter>=text.length) {
+        if (counter >= text.length) {
             counter = 0;
         }
     }
-    
-    $("#toggler").click(function(){
+
+    $("#toggler").click(function () {
         $('.sidenav').toggleClass("is-visible");
     })
+
+    $(".skill-btn").click(function () {
+        $('html,body').animate({
+                scrollTop: $("#about").offset().top
+            },
+            'slow');
+    });
+    
+    $(".about").click(function () {
+        $('html,body').animate({
+                scrollTop: $("#about").offset().top
+            },
+            'slow');
+        $(".about").addClass("current");
+        $(".home").removeClass("current");
+    });
+    
+    $(".home").click(function () {
+        $('html,body').animate({
+                scrollTop: $("#home").offset().top
+            },
+            'slow');
+        $(".about").removeClass("current");
+        $(".home").addClass("current");
+    });
 })
